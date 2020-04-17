4188a7fde76c45bff189990805ba77e7d9351428
Okrekso <semikoseker@gmail.com>
2020-04-17 18:00:10 +0300

    • lessons rebase • added fields to lesson object • added lesson to migrations and settings

---
+++ lessons/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# Create your models here.
+class Lesson(models.Model):
+    id = models.AutoField(primary_key=True, )
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    course = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    created_on = models.DateTimeField(auto_now_add=True)
+    start_on = models.DateTimeField(null=False)
+    ends_on = models.DateTimeField(null=False)
+
+    def __str__(self):
+        return self.title
