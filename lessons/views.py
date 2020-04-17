72f18ebe09c4040f6341f693a61ba4de670a2579
Okrekso <semikoseker@gmail.com>
2020-04-17 18:26:16 +0300

    • created serializer for lessons • fixed model • added lesson view

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from rest_framework import permissions
+
 # Create your views here.
+from lessons.models import Lesson
+from lessons.serializers import LessonSerializer
+
+
+class LesonViewSet(viewsets.ModelViewSet):
+    queryset = Lesson.objects.all().order_by('-start_on')
+    serializer_class = LessonSerializer
+    permission_classes = [permissions.IsAuthenticated]
