d43a88e7f31d06a475409ffcb34073511b9f1e06
Okrekso <semikoseker@gmail.com>
2020-04-21 18:11:07 +0300

    added type field to users fixed date_joined to auto_create method

--- users/models.py
+++ users/models.py
@@ -1,13 +1,19 @@
+from enum import Enum
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 from users.managers import CustomUserManager
+from django.utils.timezone import utc, now
 
 
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField('email address', unique=True)
+    TYPES = (("ADM", "Admin"), ("STD", "Student"), ("TCH", "Teacher"))
+    type = models.CharField(choices=TYPES, max_length=50, default="STD")
+    date_joined = models.DateField(null=False, auto_now=True, auto_created=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
