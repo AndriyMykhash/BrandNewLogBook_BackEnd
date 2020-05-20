c751add4db0a5732a3f90377ad509cc18db1c8ff
Okrekso <semikoseker@gmail.com>
2020-05-20 20:23:33 +0300

    now with tokens be returns user id

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from abc import ABC
+
 import jwt
 from django.contrib.auth import user_logged_in
 from django.shortcuts import render
@@ -7,6 +9,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_jwt.serializers import jwt_payload_handler
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from brand_new_logbook_be import settings
 from users.models import CustomUser
@@ -24,36 +29,17 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def authenticate_user(request):
-    try:
-        email = request.data['email']
-        password = request.data['password']
+class TokenObtainPairWithIdSerializer(TokenObtainPairSerializer):
 
-        user = CustomUser.objects.get(email=email, password=password)
-        if user:
-            try:
-                payload = jwt_payload_handler(user)
-                token = jwt.encode(payload, settings.SECRET_KEY)
+    def validate(self, attrs):
+        data = super(TokenObtainPairWithIdSerializer, self).validate(attrs)
+        data.update({'userId': self.user.id})
 
-                user_details = {}
-                user_details['name'] = "%s %s" % (
-                    user.first_name, user.last_name)
-                user_details['token'] = token
-                user_logged_in.send(sender=user.__class__,
-                                    request=request, user=user)
-                return Response(user_details, status=status.HTTP_200_OK)
+        return data
 
-            except Exception as e:
-                raise e
-        else:
-            res = {
-                'error': 'can not authenticate with the given credentials or the account has been deactivated'}
-            return Response(res, status=status.HTTP_403_FORBIDDEN)
-    except KeyError:
-        res = {'error': 'please provide a email and a password'}
-        return Response(res)
+
+class TokenObtainPairViewWithId(TokenObtainPairView):
+    serializer_class = TokenObtainPairWithIdSerializer
 
 
 @api_view(['POST'])
@@ -66,11 +52,6 @@ def create_auth(request):
                                        name=serialized.data["name"],
                                        surname=serialized.data["surname"],
                                        type=serialized.data["type"])
-        # CustomUser.objects.create_user(email=serialized.get_value("email"),
-        #                                password=serialized.get_value("password"),
-        #                                name=serialized.get_value("name"),
-        #                                surname=serialized.get_value("surname"),
-        #                                type=serialized.get_value("type"),)
         return Response(serialized.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
