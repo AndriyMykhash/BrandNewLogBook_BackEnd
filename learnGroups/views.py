c702282760e607cd7491220a5d4d20a27751f5c7
Okrekso <semikoseker@gmail.com>
2020-05-26 20:36:24 +0300

    fixed learn group and added filters

--- learnGroups/views.py
+++ learnGroups/views.py
@@ -7,7 +7,6 @@ from rest_framework import viewsets, permissions
 from learnGroups.models import LearnGroup
 from learnGroups.serializers import LearnGroupSerializer
 
-
 class LearnGroupViewSet(viewsets.ModelViewSet):
     queryset = LearnGroup.objects.all().order_by('-start_on')
     serializer_class = LearnGroupSerializer
